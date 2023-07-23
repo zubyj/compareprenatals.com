a04ccf50118ee4877850ca452de87498aa4683f0
Zuby Javed <zubyjaved@gmail.com>
2023-07-23 15:02:09 -0700

    minor css, mui updates

--- src/components/VitaminCard.jsx
+++ src/components/VitaminCard.jsx
@@ -29,34 +29,42 @@ function VitaminCard({ vitamin, vitaminSwitches }) {
                     <Grid item xs={6}>
                         <Box display="flex" flexDirection="column" height="100%">
                             <Box mb="auto">
-                                <Typography variant="h5">{vitamin.general_info.brand_name}</Typography>
-                                {Object.keys(vitaminSwitches).map(key => {
-                                    if (vitaminSwitches[key]) {
-                                        const vitaminInfo = vitamin.vitamins.find(v => v.name.toLowerCase() === key);
-                                        if (vitaminInfo) {
-                                            return (
-                                                <Button key={key} variant="text">
-                                                    {key.charAt(0).toUpperCase() + key.slice(1)}: {vitaminInfo.amount}
-                                                </Button>
-                                            );
+                                <Button variant="outlined" color="primary">{vitamin.general_info.brand_name}</Button>
+                                <Box sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+
+                                }}>
+                                    {Object.keys(vitaminSwitches).map(key => {
+                                        if (vitaminSwitches[key]) {
+                                            const vitaminInfo = vitamin.vitamins.find(v => v.name.toLowerCase() === key);
+                                            if (vitaminInfo) {
+                                                return (
+                                                    <Button key={key} variant="text">
+                                                        {key.charAt(0).toUpperCase() + key.slice(1)}: {vitaminInfo.amount}
+                                                    </Button>
+                                                );
+                                            }
                                         }
-                                    }
-                                    return null;
-                                })}
+                                        return null;
+                                    })}
+                                </Box>
                             </Box>
                         </Box>
                     </Grid>
                     <Grid item xs={6} >
                         <Box display="flex" flexDirection="column" height="100%" gap={1}>
                             <Box mb="auto" display="flex" flexDirection="column" gap={1}>
-                                <Button variant="contained" color="secondary">{vitamin.general_info.product_name}</Button>
+                                <Button variant="outlined" color="secondary">{vitamin.general_info.product_name}</Button>
                                 <Button variant="text">Type: {vitamin.general_info.format}</Button>
                                 <Button variant="text">Serving Size: {vitamin.general_info.serving_size}</Button>
                             </Box>
-                            <Button variant="contained" onClick={handleToggleVitamins}>{showVitamins ? 'Hide Vitamins' : 'Show Vitamins'}</Button>
                         </Box>
                     </Grid>
                 </Grid>
+                <Button variant="contained" onClick={handleToggleVitamins}>{showVitamins ? 'Hide Vitamins' : 'Show Vitamins'}</Button>
+
                 <Collapse in={showVitamins}>
                     <Card sx={{ backgroundColor: 'white', border: '1px solid black', padding: '5px', marginTop: '10px' }}>
                         <Typography variant="h6" sx={{ textAlign: 'center', fontFamily: "'Arial Black', 'Helvetica Bold', sans-serif" }}>Vitamins</Typography>
