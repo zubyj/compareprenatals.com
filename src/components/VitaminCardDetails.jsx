35d262770a0fc30934f4f09ee328308713bb207c
Zuby Javed <zubyjaved@gmail.com>
2023-07-26 12:42:51 -0700

    more padding

--- src/components/VitaminCardDetails.jsx
+++ src/components/VitaminCardDetails.jsx
@@ -40,11 +40,10 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                     'minHeight': '60vh',
                     'backgroundColor': 'lightblue',
                 }} >
-                    <Typography variant="h4" color="green">{vitamin.general_info.brand_name}</Typography>
-                    <Typography variant="h5" color="secondary">{vitamin.general_info.product_name}</Typography>
+                    <Typography variant="h5" color="darkgreen">{vitamin.general_info.brand_name}</Typography>
+                    <Typography variant="h6" color="secondary">{vitamin.general_info.product_name}</Typography>
                     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
-
-                        <Box sx={{ display: 'flex', flexDirection: 'row', backgroundColor: 'green', padding: '10px', borderRadius: '10px', color: 'white' }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', backgroundColor: 'green', padding: '15px', borderRadius: '10px', color: 'white' }}>
                             <MedicationIcon sx={{ fontSize: 60 }} />
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
                                 <Card><Typography variant="h6" padding={1}>{vitamin.general_info.score || '-'}/10</Typography></Card>
@@ -60,7 +59,7 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                             </Box>
                         </Box>
                     </Box>
-                    <Button variant="contained" color="secondary" href={vitamin.general_info.url} target='_blank'>Buy for ${vitamin.general_info.price}</Button>
+                    <Button variant="contained" color="secondary" href={vitamin.general_info.url} target='_blank'>Order for ${vitamin.general_info.price}</Button>
                     <Button variant="contained" onClick={handleToggleVitamins}>{showVitamins ? 'Hide Vitamins' : 'Show Vitamins'}</Button>
                     <Collapse in={showVitamins}>
                         <Card sx={{ backgroundColor: 'white', border: '1px solid black', marginTop: '10px' }}>
