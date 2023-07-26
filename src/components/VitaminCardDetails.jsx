44ea154c203a9a0f83182f47786bbb6641bd04fd
Zuby Javed <zubyjaved@gmail.com>
2023-07-26 12:39:52 -0700

    smaller text, more padding, add mui icons

--- src/components/VitaminCardDetails.jsx
+++ src/components/VitaminCardDetails.jsx
@@ -1,6 +1,7 @@
 // VitaminCardDetails.jsx
 import React from 'react';
 import { Card, CardContent, Typography, Button, Collapse, Grid, Box, Table, TableRow, TableCell, TableBody, Modal } from '@mui/material';
+import MedicationIcon from '@mui/icons-material/Medication';
 
 function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open, handleClose }) {
     // Function to divide an array into chunks of 3 vitamins
@@ -41,19 +42,21 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                 }} >
                     <Typography variant="h4" color="green">{vitamin.general_info.brand_name}</Typography>
                     <Typography variant="h5" color="secondary">{vitamin.general_info.product_name}</Typography>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '30px' }}>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '30px' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
+
+                        <Box sx={{ display: 'flex', flexDirection: 'row', backgroundColor: 'green', padding: '10px', borderRadius: '10px', color: 'white' }}>
+                            <MedicationIcon sx={{ fontSize: 60 }} />
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-                                <Card><Typography variant="h5" padding={2}>{vitamin.general_info.score || '-'}/10</Typography></Card>
-                                <Typography variant="subtitle1">Nutrition Score</Typography>
+                                <Card><Typography variant="h6" padding={1}>{vitamin.general_info.score || '-'}/10</Typography></Card>
+                                <Typography variant="subtitle2">Nutrition Score</Typography>
                             </Box>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                <Card><Typography variant="h5" padding={2}>{vitamin.general_info.format}</Typography></Card>
-                                <Typography variant="subtitle1">Format</Typography>
+                                <Card><Typography variant="h6" padding={1}>{vitamin.general_info.format}</Typography></Card>
+                                <Typography variant="subtitle2">Format</Typography>
                             </Box>
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                <Card><Typography variant="h5" padding={2} minWidth={30} textAlign={'center'}>{vitamin.general_info.serving_size}</Typography></Card>
-                                <Typography variant="subtitle1" textAlign={'center'}>Serving Size</Typography>
+                                <Card><Typography variant="h6" padding={1} minWidth={30} textAlign={'center'}>{vitamin.general_info.serving_size}</Typography></Card>
+                                <Typography variant="subtitle2" textAlign={'center'}>Serving Size</Typography>
                             </Box>
                         </Box>
                     </Box>
