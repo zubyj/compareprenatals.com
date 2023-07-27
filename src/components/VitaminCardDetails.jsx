7f18c3f38fb001f0c8250706c3a6172f0965140f
Zuby Javed <zubyjaved@gmail.com>
2023-07-26 23:21:09 -0700

    smaller width for vitamin details components, add alerts and warnings for missing & low nutrients

--- src/components/VitaminCardDetails.jsx
+++ src/components/VitaminCardDetails.jsx
@@ -70,7 +70,7 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                         'backgroundColor': 'lightcyan',
                         'padding': '30px',
                         'borderRadius': '10px',
-                        'width': '350px',
+                        'width': '300px',
                         'textAlign': 'center'
 
                     }}>
@@ -79,7 +79,7 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                         <Typography variant="h6" color="secondary" fontWeight={700}>{vitamin.general_info.product_name}</Typography>
                     </Box>
                     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'darkgreen', padding: '15px', borderRadius: '10px', color: 'white', width: '375px' }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#222', padding: '15px', borderRadius: '10px', color: 'white', width: '300px' }}>
                             <MedicationIcon sx={{ fontSize: 60 }} />
                             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
                                 <Card>
@@ -108,13 +108,13 @@ function VitaminCardDetails({ vitamin, showVitamins, handleToggleVitamins, open,
                         </Box>
                     </Box>
                     <Button variant="contained" color="primary" style={{ width: '300px' }} href={vitamin.general_info.url} target='_blank'>Order for ${vitamin.general_info.price} from Amazon</Button>
-                    <hr />
-                    <Typography variant="body1">
-                        Vitamin Details
-                    </Typography>
-                    <Button variant="contained" color="error" style={{ width: '300px' }} onClick={handleToggleMissingNutrients}>Warning: {missingNutrients.length} Nutrients Missing</Button>
-                    <Button variant="contained" color="error" style={{ width: '300px' }} onClick={handleToggleLowNutrients}>Warning: {lowNutrients.length} Nutrients Low</Button>
                     <Button variant="contained" color="primary" style={{ width: '300px' }} onClick={handleToggleVitamins}>{showVitamins ? 'Hide Vitamins' : 'Show Vitamins'}</Button>
+                    <Alert severity="warning" style={{ width: '300px' }} onClick={handleToggleMissingNutrients}>
+                        Warning: {missingNutrients.length} Nutrients Missing
+                    </Alert>
+                    <Alert severity="error" style={{ width: '300px' }} onClick={handleToggleLowNutrients}>
+                        Warning: {lowNutrients.length} Nutrients Low
+                    </Alert>
                     <Collapse in={openMissingNutrients}>
                         <Alert severity="error">
                             <Table>
